02b9b8ed6f7839a7ccb49327a31f3d51c9291fd9
lancercd <1311282756@qq.com>
2021-06-17 00:43:14 +0800

    文档类型 基础操作

---
+++ j2ee-db/src/main/java/com/j2ee/db/service/DocumentTypeService.java
@@ -0,0 +1,69 @@
+package com.j2ee.db.service;
+
+
+import com.j2ee.db.dao.DocumentTypeMapper;
+import com.j2ee.db.domain.DocumentType;
+import com.j2ee.db.domain.DocumentTypeExample;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+/**
+ * 文档类型service  基础操作
+ */
+@Service
+public class DocumentTypeService {
+
+
+    @Resource
+    private DocumentTypeMapper documentTypeMapper;
+
+
+    /**
+     * 通过id查询
+     * @param id 文档资料id
+     * @return DocumentType
+     */
+    public DocumentType findById(Integer id) {
+        return documentTypeMapper.selectByPrimaryKey(id);
+    }
+
+
+
+    /**
+     * 查询所有类型信息
+     * @return list of DocumentType
+     */
+    public List<DocumentType> queryAll(){
+        DocumentTypeExample example = new DocumentTypeExample();
+
+        return documentTypeMapper.selectByExample(example);
+    }
+
+
+
+    /**
+     * 添加类型
+     * @param doc document
+     * @return int
+     */
+    public int add(DocumentType doc) {
+
+        return documentTypeMapper.insert(doc);
+    }
+
+
+    /**
+     * 删除(逻辑删除)
+     * @param id DocumentType id
+     * @return int
+     */
+    public int delete(Integer id) {
+        return documentTypeMapper.deleteByPrimaryKey(id);
+    }
+
+
+
+}
