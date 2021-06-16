2a8a5a5b2219685a026d1ae9df2ebdeae62d1673
lancercd <1311282756@qq.com>
2021-06-16 20:46:10 +0800

    web模块

---
+++ j2ee-web/src/main/java/com/j2ee/web/Application.java
@@ -0,0 +1,19 @@
+package com.j2ee.web;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication(scanBasePackages = {"com.j2ee.db"})
+@MapperScan("com.j2ee.db.dao")
+@EnableTransactionManagement
+@EnableCaching
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ j2ee-web/src/main/java/com/j2ee/web/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.j2ee.web.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+
+    @ResponseBody
+    @GetMapping("/index")
+    public String index(){
+        return "ok";
+    }
+}
