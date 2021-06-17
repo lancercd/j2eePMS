a310c44f736db99e2d51f69001d70f9705e251d4
lancercd <1311282756@qq.com>
2021-06-17 12:05:05 +0800

    学生选择指导老师dto

---
+++ j2ee-db/src/main/java/com/j2ee/db/dto/StuTeaChDto.java
@@ -0,0 +1,93 @@
+package com.j2ee.db.dto;
+
+
+import com.j2ee.db.domain.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+//非数据库表实体
+
+/**
+ * 学生选择指导老师dto
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StuTeaChDto {
+
+    /**
+     * id
+     */
+    Integer id;
+
+    /**
+     * 指导老师
+     */
+    Teacher teacher;
+
+    /**
+     * 学生
+     */
+    Student student;
+
+    /**
+     * 指导老师信息
+     */
+    AdviserInfo adviserInfo;
+
+    /**
+     * 学期
+     */
+    Semester semester;
+
+    /**
+     * 评阅老师
+     */
+    AppraiseTeacher appraiseTeacher;
+
+
+    /**
+     * 学生自我介绍
+     */
+    String intro;
+
+
+    /**
+     * 评阅建议
+     */
+    String suggestion;
+
+
+    /**
+     * 评阅分数
+     */
+    Integer score;
+
+
+    /**
+     * 学生上传的资料
+     */
+    Document document;
+
+
+    /**
+     * 老师是否接受成为导师
+     *      0等待回复  1同意  -1不同意
+     */
+    Short isAccept;
+
+    /**
+     * 是否逻辑删除
+     */
+    Boolean isDel;
+
+
+    /**
+     * 添加时间
+     */
+    LocalDateTime addTime;
+
+}
