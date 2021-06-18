e02570b162d3e99e58f732b5bf7bc33dc6fcffe7
lancercd <1311282756@qq.com>
2021-06-18 21:10:36 +0800

    登录注册等权限控制器

--- src/main/java/com/j2ee/controller/AuthController.java
+++ src/main/java/com/j2ee/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.j2ee.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 /**
@@ -11,4 +13,16 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class AuthController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "/auth/login";
+    }
+
+    @PostMapping("/login")
+    public String loginApi(){
+
+
+        return null;
+    }
 }
