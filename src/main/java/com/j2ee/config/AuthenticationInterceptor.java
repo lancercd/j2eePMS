701ee6370b872c090d8d5d8c64061f3bfa303d0e
lancercd <1311282756@qq.com>
2021-06-19 22:49:34 +0800

    登录拦截器  参数解析器 成功

--- src/main/java/com/j2ee/config/AuthenticationInterceptor.java
+++ src/main/java/com/j2ee/config/AuthenticationInterceptor.java
@@ -1,24 +1,41 @@
 package com.j2ee.config;
 
 
-import com.j2ee.annotation.Login;
+import com.j2ee.annotation.*;
+import com.j2ee.dto.LoginType;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Configuration
 public class AuthenticationInterceptor implements HandlerInterceptor {
 
 
+    private static final Set<Object> LOGIN_ANNOTATIONS;
+
+    static {
+        LOGIN_ANNOTATIONS = new HashSet<>(5);
+        LOGIN_ANNOTATIONS.add(StudentLogin.class);
+        LOGIN_ANNOTATIONS.add(TeacherLogin.class);
+        LOGIN_ANNOTATIONS.add(TeachingSecretaryLogin.class);
+        LOGIN_ANNOTATIONS.add(AdminLogin.class);
+        LOGIN_ANNOTATIONS.add(Login.class);
+    }
+
+
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
 
         if(!(handler instanceof HandlerMethod)){
             return true;
@@ -26,16 +43,108 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
 
         HandlerMethod handlerMethod=(HandlerMethod)handler;
         Method method=handlerMethod.getMethod();
+        if(method.getName().equals("errorHtml")){
+            return true;
+        }
 
-        if (method.isAnnotationPresent(Login.class)) {
-            HttpSession session = request.getSession();
+        Annotation[] declaredAnnotations = method.getDeclaredAnnotations();
 
-            System.out.println(session.getAttribute("uid"));
-            System.out.println(session.getAttribute("username"));
-
-
+        Object annotation = detectHasLoginAnnotations(declaredAnnotations);
+        if (annotation != null) {
+            if(!handle(annotation, request.getSession())){
+                forwardToLogin(request, response);
+            }
         }
 
         return true;
+    }
+
+
+    /**
+     * 判断登录的类型是否与方法上的注解一致
+     * @param annotation  方法上的登录类型注解
+     * @param session     session
+     * @return  boolean
+     */
+    private boolean handle(Object annotation, HttpSession session) {
+
+        Integer uid = (Integer) session.getAttribute("uid");
+        String username = (String) session.getAttribute("username");
+        LoginType type = (LoginType) session.getAttribute("type");
+        if(uid == null || username == null || type == null){
+            return false;
+        }
+
+
+        if (annotation instanceof StudentLogin) {
+            if(type != LoginType.STUDENT) {
+                return false;
+            }
+
+        }else if (annotation instanceof TeacherLogin) {
+            if(type != LoginType.TEACHER) {
+                return false;
+            }
+
+        }else if (annotation instanceof AdminLogin) {
+            if(type != LoginType.ADMIN) {
+                return false;
+            }
+
+        }else if (annotation instanceof TeachingSecretaryLogin) {
+            if(type != LoginType.SECRETARY) {
+                return false;
+            }
+
+        }else if (annotation instanceof Login) {
+            if(type != LoginType.STUDENT) {
+                return false;
+            }
+
+            LoginType[] types = ((Login)annotation).type();
+
+            for (LoginType value : types) {
+                if(value == type){
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+
+
+    /**
+     * 检测注解中是否包含登录注解
+     * @param target 目标注解数组
+     * @return annotation || null
+     */
+    private Object detectHasLoginAnnotations(Annotation[] target){
+
+        for (Annotation annotation : target) {
+            if (LOGIN_ANNOTATIONS.contains(annotation.annotationType())) {
+                return annotation;
+            }
+        }
+
+        return null;
+    }
+
+
+
+    /**
+     * 跳转登录页面
+     * @param request           req
+     * @param response          resp
+     * @throws ServletException ServletException
+     * @throws IOException      IOException
+     */
+    private void forwardToLogin(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws ServletException, IOException {
+
+        request.getRequestDispatcher("/login").forward(request, response);
     }
 }
--- src/main/java/com/j2ee/config/ProjWebMvcConfiguration.java
+++ src/main/java/com/j2ee/config/ProjWebMvcConfiguration.java
@@ -1,10 +1,12 @@
 package com.j2ee.config;
 
 
+import com.j2ee.annotation.support.LoginInfoHandlerMethodArgumentResolver;
 import com.j2ee.annotation.support.LoginUidHandlerMethodArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,9 +15,6 @@ import java.util.List;
 
 @Configuration
 public class ProjWebMvcConfiguration implements WebMvcConfigurer {
-
-    @Autowired
-    private LoginUidHandlerMethodArgumentResolver loginUidHandlerMethodArgumentResolver;
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -27,6 +26,12 @@ public class ProjWebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(loginUidHandlerMethodArgumentResolver);
+        resolvers.add(new LoginUidHandlerMethodArgumentResolver());
+        resolvers.add(new LoginInfoHandlerMethodArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthenticationInterceptor()).addPathPatterns("/**");
     }
 }
