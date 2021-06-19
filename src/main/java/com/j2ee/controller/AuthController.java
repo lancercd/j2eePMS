97e476214680c67fbf4452576dca83ef7d0a3b18
lancercd <1311282756@qq.com>
2021-06-19 22:50:00 +0800

    登录控制器   测试注解  测试参数解析完成

--- src/main/java/com/j2ee/controller/AuthController.java
+++ src/main/java/com/j2ee/controller/AuthController.java
@@ -1,10 +1,15 @@
 package com.j2ee.controller;
 
 
+import com.j2ee.annotation.*;
+import com.j2ee.dto.LoginType;
+import com.j2ee.dto.UserLoginInfo;
 import com.j2ee.utils.ResponseUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
@@ -35,7 +40,7 @@ public class AuthController {
         if(tmpUsername.equals(username)){
             if(tmpPwd.equals(pwd)){
                 session.setAttribute("uid", 1);
-                session.setAttribute("type", 1);
+                session.setAttribute("type", LoginType.STUDENT);
                 session.setAttribute("username", username);
                 return ResponseUtil.ok("登录成功");
             }
@@ -45,5 +50,28 @@ public class AuthController {
 
         return ResponseUtil.fail(-1, "用户名不存在");
 
+    }
+
+
+    /**
+     * 登录注解测试
+     *        参数解析测试
+     *        完成
+     * @param uid       uid参数解析测试
+     * @param userInfo  userInfo参数解析测试成功
+     * @return String
+     */
+//    @StudentLogin
+    @Login(type = {LoginType.ADMIN})
+    @RequestMapping("/auth/test")
+    public String authTest(@LoginUid Integer uid, @LoginInfo UserLoginInfo userInfo){
+        System.out.println("---------------------");
+        System.out.println(uid);
+        System.out.println(userInfo);
+        System.out.println("---------------------");
+
+
+
+        return "setNews";
     }
 }
