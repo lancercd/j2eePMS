c5225e47895a071c88a909bac299867d8b36e2c5
lancercd <1311282756@qq.com>
2021-06-20 19:24:36 +0800

    学生选择导师 (没做完)

--- src/main/java/com/j2ee/controller/StudentController.java
+++ src/main/java/com/j2ee/controller/StudentController.java
@@ -2,7 +2,22 @@ package com.j2ee.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("student")
 public class StudentController {
+
+    /**
+     * 学生选择导师页面
+     * @return String
+     */
+    @GetMapping("/selectAdviser")
+    public String addTeacher(){
+        return "student/selectAdviser";
+    }
+
+
+
 }
