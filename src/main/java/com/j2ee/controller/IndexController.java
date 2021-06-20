58c2c463d9966bdd9b67bccfe9a1c5492a6d4bf2
zhangsan <zhangsan@qq.com>
2021-06-20 12:02:58 +0800

    合并 newsEdit

--- src/main/java/com/j2ee/controller/IndexController.java
+++ src/main/java/com/j2ee/controller/IndexController.java
@@ -1,40 +1,84 @@
-package com.j2ee.controller;
+package com.j2ee.web.controller;
 
 
+import com.j2ee.core.utils.ResponseUtil;
+import com.j2ee.db.dao.NewsMapper;
+import com.j2ee.db.domain.News;
+import com.j2ee.db.service.NewsService;
+import com.j2ee.db.service.StudentService;
+import com.j2ee.web.service.StudentTeacherChoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-
-/**
- * 首页控制器
- *      该控制器只有一个方法
- *                  index()
- *      TODO 其余方法请写在对应模块
- *      TODO 如果是测试  请随意  单后期请删除
- */
 @Controller
 public class IndexController {
+    @Autowired
+    private StudentTeacherChoiceService STCS;
+
+    @Autowired
+    private StudentService studentService;
+
+    /**
+     * 戴林赵毅负责代码
+     * 开始区域
+     */
+    @Autowired
+    private NewsService newsService;
 
     @GetMapping("/setTeacher")
     public String setTeacher() {
-
-        return "setTeacher";
-    }
-
-    @GetMapping("/index")
-    public String index(){
-        return "index";
+        return "/setTeacher";
     }
 
     @GetMapping("/setNews")
     public String setNews(){
-        return "setNews";
+        return "/setNews";
     }
 
     @GetMapping("/teachingSecretary")
     public String teachingSecretary(){
-        return "teachingSecretary";
+        return "/teachingSecretary";
     }
 
+    @PostMapping("/addOrUpdate")
+    public Object addOrUpdate(News news){
+        if(news.getId() == null){
+            newsService.insert(news);
+        }
+        else {
+            newsService.update(news);
+        }
+        return "/setNews";
+    }
+
+    /**
+     * 戴林赵毅
+     * 结束区域
+     */
+
+
+
+    @ResponseBody
+    @GetMapping("/index")
+    public String index(){
+        return "ok";
+    }
+
+    @ResponseBody
+    @GetMapping("/query")
+    public Object queryAll(){
+
+        return ResponseUtil.ok(studentService.queryAll());
+    }
+
+    @ResponseBody
+    @GetMapping("/STC")
+    public Object querySTC(){
+
+        return ResponseUtil.ok(STCS.findDtoById(1));
+    }
 
 }
