9f95c44f0c8db79fac7f565b032ee579da93c4ee
lancercd <1311282756@qq.com>
2021-06-21 18:33:59 +0800

    学生选择指导老师 接口添加

--- src/main/java/com/j2ee/controller/StudentController.java
+++ src/main/java/com/j2ee/controller/StudentController.java
@@ -4,6 +4,8 @@ package com.j2ee.controller;
 
 import com.j2ee.annotation.LoginUid;
 import com.j2ee.annotation.StudentLogin;
+import com.j2ee.db.domain.StuTeaCh;
+import com.j2ee.db.service.AdviserInfoService;
 import com.j2ee.utils.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.j2ee.service.StudentTeacherChoiceService;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -23,9 +26,14 @@ public class StudentController {
     private StudentTeacherChoiceService studentTeacherChoiceService;
 
 
+    @Autowired
+    private AdviserInfoService adviserInfoService;
+
+
 
     /**
-     * 学生选择导师页面
+     * 学生选择导师页面(根据学期选择)
+     * @param semesterId 学期id
      * @return String
      */
     @StudentLogin
@@ -46,13 +54,50 @@ public class StudentController {
     }
 
 
-
+    /**
+     * 学生选择指导老师 api
+     * @param uid       uid
+     * @param adviserId 指导老师信息
+     * @return Json
+     */
     @StudentLogin
+    @ResponseBody
     @PostMapping("/adviser/add")
     public Object selectAdviser(@LoginUid Integer uid, Integer adviserId){
 
+        //写的好**乱啊  不想改了  就这样吧
 
-        return ResponseUtil.ok("uid:" + uid + "  adviserId:" + adviserId);
+        if (adviserId == null) {
+            return ResponseUtil.fail(400, "id不能为空!");
+        }
+
+        StuTeaCh choiceInfo = studentTeacherChoiceService.findStuTeachByStuIdAndTeaId(adviserId);
+
+        if (choiceInfo == null) {
+            //没选择过该指导老师
+
+            int res = studentTeacherChoiceService.addSelectInfo(adviserInfoService.findById(adviserId),uid);
+
+            if(res == 0){
+                return ResponseUtil.fail("添加数据库失败");
+            }
+
+        }else{
+            //选择过该指导老师
+            Byte isAccept = choiceInfo.getIsAccept();
+            String msg = null;
+            if(isAccept == 0){
+                msg = ",请耐心等待老师回复哦";
+            }else if (isAccept == 1){
+                msg = "!";
+            }else{
+                msg = ",但老师位同意!";
+            }
+
+            return ResponseUtil.fail("您已选择过该老师了" + msg);
+        }
+
+        return ResponseUtil.ok("添加成功!");
     }
 
 
