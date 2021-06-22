2fe1cf05178544588e7a08215bda04701221bc2e
67catl <2235096287@qq.com>
2021-06-22 18:01:30 +0800

    secretary check

---
+++ src/main/resources/static/js/admin/adUser/adSecretary.js
@@ -0,0 +1,46 @@
+import request from '../../utils/request.js';
+import Message from '../../utils/Message.js';
+
+$('#seach').click(function () {
+    const number = $("#number").val();
+    window.location.href = "/admin/adUser/secretary?id="+number;
+});
+
+$('#add').click(function () {
+    window.location.href = "/admin/adUser/addSecretary";
+});
+
+const delRoute = '/admin/adUser/del/secretary/';
+
+const selectBtns = document.querySelectorAll('.select-adviser-btn');
+const message = new Message();
+selectBtns.forEach((el, index) => {
+    el.addEventListener('click', (e) => {
+        const target = e.target;
+        const id = target.dataset.id;
+        request({
+            url: delRoute + id,
+            type: 'POST',
+            data: {
+                semesterId: id
+            }
+        }).then(
+            (data) => {
+                console.log(data);
+                message.show({
+                    type: 'success',
+                    msg: data.data,
+                    closeable: true,
+                });
+                window.location.reload();
+            },
+            (msg) => {
+                message.show({
+                    type: 'info',
+                    msg: msg.msg,
+                    closeable: true,
+                });
+            });
+
+    })
+})
